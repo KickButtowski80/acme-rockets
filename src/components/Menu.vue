13d5a60ea480aff75c02271c7928496c70c30d30
kickbuttowski80 <pazpaz25@gmail.com>
2023-04-13 13:35:07 -0700

    responisve implementation continue

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,22 +1,52 @@
 <template>
     <div class="flex justify-between bg-green-800
-                    text-gray-300">
-        <div class="flex items-center p-4">
-            <img src="/favicon.ico" alt="">
-            <h2 class="text-2xl font-semibold">
+                text-gray-300">
+        <div class="flex items-center py-4">
+            <img class="w-8 ml-3" src="/favicon.ico" alt="aceme rocket logo">
+            <h2 class="text-base md:text-2xl font-bold
+                        ml-3">
                 Aceme Rockets
             </h2>
         </div>
-        <ol class="flex items-center [&>*]:p-4
-                    [&>*]:text-xl font-bold">
+        <ol class=" hidden m-2
+                    sm:flex sm:items-center 
+                    [&>*]:w-32 [&>*]:text-xs
+                    md:[&>*]:text-base md:font-semibold">
             <li>Our Rockets</li>
             <li>Testmonials</li>
             <li>Contact Us</li>
         </ol>
+        <div class="block border-2 sm:hidden
+                    m-2 rounded-2xl" @click="toggleMenu">
+            <hr class="w-6 h-2 m-2 bg-white" />
+            <hr class="w-6 h-2 m-2 bg-white" />
+            <hr class="w-6 h-2 m-2 bg-white" />
+        </div>
+
     </div>
+    <ol :class="`${showingStatus}`" 
+        class=" bg-green-800 float-right
+                h-screen overflow-x-hidden
+                flex flex-col items-end 
+                [&>*]:m-1 xs:[&>*]:text-lg
+                md:font-semibold xs:hidden sm:[&>*]:hidden">
+        <li>Our Rockets</li>
+        <li>Testmonials</li>
+        <li>Contact Us</li>
+    </ol>
 </template>
 <script>
 export default {
+    data() {
+        return {
+            showingStatus: 'hidden',
+        }
+    },
+    methods: {
+        toggleMenu() {
+            this.showingStatus = this.showingStatus === 'hidden' ? 'block' : 'hidden'
+        }
 
+    }
 }
 </script>
