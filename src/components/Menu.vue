cd154f8d2084ad2646c203b3db9a146d0141fcea
kickbuttowski80 <pazpaz25@gmail.com>
2023-04-12 20:14:21 -0700

    priminlary desgin

---
+++ src/components/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex justify-between bg-green-800
+                    text-gray-300">
+        <div class="flex items-center p-4">
+            <img src="/favicon.ico" alt="">
+            <h2 class="text-2xl font-semibold">
+                Aceme Rockets
+            </h2>
+        </div>
+        <ol class="flex items-center [&>*]:p-4
+                    [&>*]:text-xl font-bold">
+            <li>Our Rockets</li>
+            <li>Testmonials</li>
+            <li>Contact Us</li>
+        </ol>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
