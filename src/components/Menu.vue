2afcfba376aa3b5d63455e70a2e849cc0cb47600
kickbuttowski80 <pazpaz25@gmail.com>
2023-04-20 13:10:39 -0700

    added dark mode btn

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,73 +1,78 @@
 <template>
     <div class="flex justify-between bg-green-800
-               text-gray-300">
+                   text-gray-300">
         <div class="flex items-center py-4">
             <img class="w-8 ml-3" src="/favicon.ico" alt="aceme rocket logo">
             <h2 class="text-2xl md:text-3xl font-bold
-                            ml-3">
+                                ml-3">
                 Acme Rockets
             </h2>
         </div>
         <nav class="hidden m-2
-                    sm:flex sm:items-center 
-                    [&>*]:w-32 [&>*]:text-lg
-                    md:[&>*]:text-xl md:font-semibold
-                    hover:[&>*]:text-xl hover:[&>*]:border hover:[&>*]:border-sky-500
-                    hover:[&>*]:rounded-2xl hover:[&>*]:p-2 hover:[&>*]:mx-2
-                    active:[&>*]:bg-blue-700 
-                    focus:[&>*]:outline-none focus:[&>*]:ring focus:[&>*]:ring-blue-900
-                    focus:[&>*]:rounded-full focus:[&>*]:p-2                  
-                  "
-                  >
-            <button >Our Rockets</button>
+                        sm:flex sm:items-center 
+                        [&>*]:w-32 [&>*]:text-lg
+                        md:[&>*]:text-xl md:font-semibold
+                        hover:[&>*]:text-xl hover:[&>*]:border hover:[&>*]:border-sky-500
+                        hover:[&>*]:rounded-2xl hover:[&>*]:p-2 hover:[&>*]:mx-2
+                        active:[&>*]:bg-blue-700 
+                        focus:[&>*]:outline-none focus:[&>*]:ring focus:[&>*]:ring-blue-900
+                        focus:[&>*]:rounded-full focus:[&>*]:p-2     
+                        last:none             
+                      ">
+            <button>Our Rockets</button>
             <button>Testmonials</button>
             <button>Contact Us</button>
+            <ThemeBtn/>
         </nav>
-        <div  :class="`${showingHamburger}`"
-        class=" border-2  sm:hidden
-                m-2 rounded-2xl border-red-900
-                [&>*]:bg-blue-400 [&>*]:w-6 [&>*]:h-1
-                [&>*]:m-2
-                hover:[&>*]:w-12 " @click="toggleMenu">
-            <hr/>
-            <hr/>
-            <hr/>
+        
+        <div :class="`${showingHamburger}`" class=" border-2  sm:hidden
+                    m-2 rounded-2xl border-red-900
+                    [&>*]:bg-blue-400 [&>*]:w-6 [&>*]:h-1
+                    [&>*]:m-2
+                    hover:[&>*]:w-12 " @click="toggleMenu">
+            <hr />
+            <hr />
+            <hr />
         </div>
-       <div  :class="`${showingCross}`"
-       class=" border-2 sm:hidden
-                m-2 rounded-2xl border-red-900 [&>*]:bg-blue-400
-                [&>*]:w-10 [&>*]:h-1
-                flex items-center relative
-                 "
-                @click="toggleMenu">
-        <hr class="rotate-45 absolute">
-        <hr class="-rotate-45">
-       </div>
-    </div>    
-    <nav :class="`${showingMenu}`"
-        class="absolute right-0 bg-green-800       
-            flex flex-col 
-            md:font-semibold sm:hidden sm:[&>*]:hidden
-            [&>*]:m-1 [&>*]:text-lg text-gray-300
-            hover:[&>*]:text-xl hover:[&>*]:border hover:[&>*]:border-sky-500
-            hover:[&>*]:rounded-2xl hover:[&>*]:p-2 hover:[&>*]:mx-2
-            active:[&>*]:bg-blue-700 
-            focus:[&>*]:outline-none focus:[&>*]:ring focus:[&>*]:ring-blue-900
-            focus:[&>*]:rounded-full focus:[&>*]:p-2 
-            h-screen
-            ">
-        <button >Our Rockets</button>
-        <button >Testmonials</button>
-        <button >Contact Us</button>
+        <div :class="`${showingCross}`" class=" border-2 sm:hidden
+                    m-2 rounded-2xl border-red-900 [&>*]:bg-blue-400
+                    [&>*]:w-10 [&>*]:h-1
+                    flex items-center relative
+                     " @click="toggleMenu">
+            <hr class="rotate-45 absolute">
+            <hr class="-rotate-45">
+        </div>
+    </div>
+    <nav :class="`${showingMenu}`" class="absolute right-0 bg-green-800       
+                flex flex-col 
+                md:font-semibold sm:hidden sm:[&>*]:hidden
+                [&>*]:m-1 [&>*]:text-lg text-gray-300
+                hover:[&>*]:text-xl hover:[&>*]:border hover:[&>*]:border-sky-500
+                hover:[&>*]:rounded-2xl hover:[&>*]:p-2 hover:[&>*]:mx-2
+                active:[&>*]:bg-blue-700 
+                focus:[&>*]:outline-none focus:[&>*]:ring focus:[&>*]:ring-blue-900
+                focus:[&>*]:rounded-full focus:[&>*]:p-2 
+                h-screen
+                ">
+        <button>Our Rockets</button>
+        <button>Testmonials</button>
+        <button>Contact Us</button>
+        <hr>
+        <ThemeBtn />
     </nav>
 </template>
 <script>
+// import ThemeBtn from './ThemeBtn.vue'
 export default {
+    // components:{
+    //     ThemeBtn,
+    // },
     data() {
         return {
             showingMenu: 'hidden',
             showingHamburger: 'block',
-            showingCross:'hidden',
+            showingCross: 'hidden',
+
         }
     },
     methods: {
---
+++ src/components/ThemeBtn.vue
@@ -0,0 +1,37 @@
+<template lang="">    
+    <button 
+    @click='toggleTheme'
+    >
+    {{ themeStatus }}
+    </button>    
+</template>
+<script>
+export default {
+    data() {
+        return {
+            themeStatus: 'light'
+        }
+    },
+    mounted() {
+        localStorage.theme = 'dark'
+
+    },
+    methods: {
+        toggleTheme() {
+            if (this.themeStatus === 'dark' || (!('theme' in localStorage)
+            && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.remove('dark')
+                this.themeStatus = 'light'
+                localStorage.theme = 'light'
+            } else {
+                document.documentElement.classList.add('dark')
+                this.themeStatus = 'dark'
+                localStorage.theme = 'dark'
+               
+            }
+       
+           
+        }
+    }
+}
+</script>
