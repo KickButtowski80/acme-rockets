c405756e22bbf63f9ff6d50c657be98f87eb6ef3
kickbuttowski80 <pazpaz25@gmail.com>
2023-04-24 13:29:05 -0700

    added des

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,11 +4,17 @@
                         flex flex-col justify-center items-center
                         sm:flex-row' >
            <section class='font-bold text-2xl md:text-4xl
-                           '>
+                           max-w-xs '>
                We Boldy Go <br>
               <span class='text-purple-500'>Where No Rocket </span> <br>  
                Has Gone <br>
                Before...
+               <section class='text-sm text-gray-600 mt-10 mb-5'>
+                We're building rockets for the next <br>
+                century today. From the moon to <br>
+                Mars, Jupiter and beyound ...🚀<br>
+                Think Acme Rockets.
+               </section>
            </section> 
            <section>
                <img src="../assets/img/rocketdab.png" alt="rocketman dab"
