11138ad401fad8a13ae30e05a8dcdd665588969e
kickbuttowski80 <pazpaz25@gmail.com>
2023-04-24 13:50:33 -0700

    some text and text h adjustments

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,15 +1,15 @@
 <template lang="">
-    <section class='mx-auto max-w-xl lg:max-w-4xl'>
+    <section class='mx-auto max-w-lg lg:max-w-4xl'>
         <section class='h-screen
-                        flex flex-col justify-center items-center
+                        flex flex-col items-center mt-2
                         sm:flex-row' >
            <section class='font-bold text-2xl md:text-4xl
-                           max-w-xs '>
+                           max-w-xs leading-normal '>
                We Boldy Go <br>
               <span class='text-purple-500'>Where No Rocket </span> <br>  
                Has Gone <br>
                Before...
-               <section class='text-sm font-normal text-gray-600 mt-10 mb-5'>
+               <section class='text-sm font-normal text-gray-600 my-5'>
                 We're building rockets for the next <br>
                 century today. From the moon to <br>
                 Mars, Jupiter and beyound ...🚀<br>
@@ -18,7 +18,7 @@
            </section> 
            <section>
                <img src="../assets/img/rocketdab.png" alt="rocketman dab"
-               class="w-72">
+               class="w-72 ">
            </section>
          
         </section>
