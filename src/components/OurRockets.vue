f2c3b67c325da4b0d769fe7964c67b16eca9827f
kickbuttowski80 <pazpaz25@gmail.com>
2023-04-25 16:26:42 -0700

    boilerplate

---
+++ src/components/OurRockets.vue
@@ -0,0 +1,13 @@
+<template lang="">
+ <section>
+    <h2 class="text-4xl font-semibold text-center ">Our Rockets</h2>
+ </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
