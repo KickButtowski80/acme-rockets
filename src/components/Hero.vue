751f1986406a683f7c51ce3fa552f8b9ddb985e6
kickbuttowski80 <pazpaz25@gmail.com>
2023-04-25 15:43:10 -0700

    changed texts color

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -6,19 +6,21 @@
             <img src="../assets/img/rocketdab.png" alt="rocketman dab"
             class="w-72 ">
         </section>
-    <section class='font-bold text-2xl md:text-4xl
+    <article class='font-bold text-2xl md:text-4xl
                     max-w-xs leading-normal '>
         We Boldy Go <br>
-        <span class='text-purple-500'>Where No Rocket </span> <br>  
+        <span class='text-indigo-700 dark:text-indigo-300'>
+        Where No Rocket
+        </span> <br>  
         Has Gone <br>
         Before...
-        <section class='text-sm font-normal text-gray-600 my-5'>
+        <section class='my-5 text-sm font-normal text-slate-600 '>
         We're building rockets for the next <br>
         century today. From the moon to <br>
         Mars, Jupiter and beyound ...🚀<br>
         Think Acme Rockets.
         </section>
-        </section>         
+     </article>         
     </section>
 </template>
 <script>
