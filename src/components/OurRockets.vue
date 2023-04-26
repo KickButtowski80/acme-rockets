a254ae294131e5b0392e2e2a0c6f8f2d7b4783a1
kickbuttowski80 <pazpaz25@gmail.com>
2023-04-25 17:17:48 -0700

    populated the cmp

--- src/components/OurRockets.vue
+++ src/components/OurRockets.vue
@@ -1,6 +1,34 @@
 <template lang="">
  <section>
-    <h2 class="text-4xl font-semibold text-center ">Our Rockets</h2>
+    <h2 class="text-4xl font-semibold text-center
+              my-10 ">Our Rockets</h2>
+ </section>
+ <section class='flex flex-col items-center
+                 sm:flex-row  sm:justify-evenly my-5'>
+    <div class=" border-2 border-gray-500 rounded-lg
+               flex flex-col items-center 
+                mx-5 my-5 w-2/3 p-2">
+         <img src="/src/assets/img/rocketman.png" alt="rocket man"
+         class="w-1/3">
+         <h5>Explorer</h5>
+         <p>$</p>
+    </div>
+    <div class=" border-2 border-gray-500 rounded-lg
+               flex flex-col items-center
+               mx-5 my-5 w-2/3 p-2">
+         <img src="/src/assets/img/rocketride.png" alt="rocket man"
+         class="w-1/3">
+         <h5>Advanturer</h5>
+         <p>$$</p>
+    </div>
+    <div class=" border-2 border-gray-500 rounded-lg
+               flex flex-col items-center
+               mx-5 my-5 w-2/3 p-2">
+         <img src="/src/assets/img/rocketlaunch.png" alt="rocket man"
+         class="w-1/3">
+         <h5>Infinity</h5>
+         <p>$$$</p>
+    </div>
  </section>
 </template>
 <script>
