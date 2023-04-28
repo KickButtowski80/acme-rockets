a90fbf1a9c1de8d5df5c4b7c1dd592dbaeaded16
kickbuttowski80 <pazpaz25@gmail.com>
2023-04-28 12:27:35 -0700

    refactored

--- src/components/OurRockets.vue
+++ src/components/OurRockets.vue
@@ -1,18 +1,17 @@
 <template lang="">
  <TitleSction title="Our Rockets"/> 
  <section class='flex flex-col items-center
-                 sm:flex-row  sm:justify-evenly my-5'>
+                 sm:flex-row  sm:justify-evenly my-20'>
     <div v-for='{img,name,price} in rocketsInfo' :key='price'
     class=" border-2 border-gray-500 rounded-3xl
           shadow-xl
           flex flex-col items-center 
-           mx-5 my-5 w-2/3 p-2">    
+           mx-5 my-5 w-2/3 p-2">   
               
          <img :src='img' :alt='name' class="w-1/3">
          <h5>{{name}}</h5> 
-         <p  class='sm:block md:hidden'>{{price.amount}}</p>    
-         <p  class='hidden md:block'>{{price.explanation}}</p> 
-         
+         <p  class='md:hidden'>{{price.amount}}</p>    
+         <p  class='hidden md:block'>{{price.explanation}}</p>          
     </div>
    
  </section>
