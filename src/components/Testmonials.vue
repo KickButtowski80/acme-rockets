9c9d79babbcfbaad894cd549025f03d1ea79895e
kickbuttowski80 <pazpaz25@gmail.com>
2023-04-28 13:21:06 -0700

    changed reso

--- src/components/Testmonials.vue
+++ src/components/Testmonials.vue
@@ -1,6 +1,6 @@
 <template lang="">
   <TitleSction title="Testmonials"/> 
-  <section class='flex flex-col md:flex-row'>
+  <section class='flex flex-col lg:flex-row'>
     <Testominal>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, soluta laudantium atque aliquid labore repudiandae? Eveniet placeat cupiditate, repudiandae impedit sed qui quasi quas aut eaque enim nobis quaerat porro.
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia fugit fugiat ab eaque soluta nobis corporis sit, enim maxime molestias architecto vitae officiis aperiam delectus blanditiis libero repellat ipsa nisi.
