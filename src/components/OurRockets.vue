0131fe6e29f08bbc4bddeb2e3dfac25c826db049
kickbuttowski80 <pazpaz25@gmail.com>
2023-04-27 18:49:19 -0700

    showing amount and expl in diff resoul

--- src/components/OurRockets.vue
+++ src/components/OurRockets.vue
@@ -2,16 +2,17 @@
  <TitleSction title="Our Rockets"/> 
  <section class='flex flex-col items-center
                  sm:flex-row  sm:justify-evenly my-5'>
-    <div v-for='({img,name,price}) in rocketsInfo' :key='price'
+    <div v-for='{img,name,price} in rocketsInfo' :key='price'
     class=" border-2 border-gray-500 rounded-3xl
           shadow-xl
           flex flex-col items-center 
-           mx-5 my-5 w-2/3 p-2">
-                
-         <img :src='img' :alt='name'
-         class="w-1/3">
-         <h5>{{name}}</h5>
-         <p>{{price}}</p>
+           mx-5 my-5 w-2/3 p-2">                
+         <img :src='img' :alt='name' class="w-1/3">
+         <h5>{{name}}</h5> 
+        
+          <p  class='sm:block md:hidden'>{{price.amount}}</p>    
+          <p  class='hidden md:block'>{{price.explanation}}</p> 
+         
     </div>
    
  </section>
@@ -24,17 +25,17 @@ export default {
                     {
                          img: '/src/assets/img/rocketman.png',
                          name: 'Explorer',
-                         price: '$'
+                         price: { amount: '$', explanation: 'Affordable Exploration' }
                     },
                     {
                          img: '/src/assets/img/rocketride.png',
                          name: 'Advanturer',
-                         price: '$$'
+                         price: { amount: '$$', explanation: 'Best Selling Rockets!' }
                     },
                     {
                          img: '/src/assets/img/rocketlaunch.png',
                          name: 'Infinity',
-                         price: '$$$'
+                         price: { amount: '$$$', explanation: 'Luxury Starship' }
                     },
                ]
           }
