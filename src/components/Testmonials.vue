5b72f7b194510baaf57d38c1535b343121d95cdc
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-03 14:36:39 -0700

    Added A Testmonial

--- src/components/Testmonials.vue
+++ src/components/Testmonials.vue
@@ -1,11 +1,17 @@
 <template lang="">
   <TitleSction title="Testmonials"/> 
   <section class='flex flex-col 
-                  lg:grid lg:grid-cols-2               
+                  lg:grid lg:grid-cols-2            
                  '>
+    <Testominal>
+        Acme
+        has always been three-linefor me. Their Exploer rocket arrived in a wooden
+        crate as expected.
+        Lif-long Customer! A++ shopping experince 
+     </Testominal>
     <Testominal v-for='t in 4'>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, soluta laudantium atque aliquid labore repudiandae? 
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia fugit fugiat ab eaque soluta nobis corporis sit, enim
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia fugit fugiat ab eaque soluta nobis corporis sit.enim kiko
       <template v-slot:author>
         Izak T
       </template>
@@ -15,7 +21,7 @@
 <script>
 import Testominal from '../slots/Testominal.vue';
 export default {
-  components:{
+  components: {
     Testominal,
   }
 }
