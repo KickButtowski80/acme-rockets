595cad6cb9f2c5420ceae7a40a1ab15229dadbe4
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-02 18:34:46 -0700

    used for loop

--- src/components/Testmonials.vue
+++ src/components/Testmonials.vue
@@ -1,20 +1,13 @@
 <template lang="">
   <TitleSction title="Testmonials"/> 
   <section class='flex flex-col lg:flex-row'>
-    <Testominal>
+    <Testominal v-for='t in 4'>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, soluta laudantium atque aliquid labore repudiandae? Eveniet placeat cupiditate, repudiandae impedit sed qui quasi quas aut eaque enim nobis quaerat porro.
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia fugit fugiat ab eaque soluta nobis corporis sit, enim maxime molestias architecto vitae officiis aperiam delectus blanditiis libero repellat ipsa nisi.
       <template v-slot:author>
         Izak T
       </template>
-    </Testominal>
-    <Testominal>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, soluta laudantium atque aliquid labore repudiandae? Eveniet placeat cupiditate, repudiandae impedit sed qui quasi quas aut eaque enim nobis quaerat porro.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia fugit fugiat ab eaque soluta nobis corporis sit, enim maxime molestias architecto vitae officiis aperiam delectus blanditiis libero repellat ipsa nisi.
-      <template v-slot:author>
-        IT
-      </template>
-    </Testominal>    
+    </Testominal> 
   </section>
 </template>
 <script>
