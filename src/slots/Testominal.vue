7fbc48b08556a8666c1ab3b2cac0b37df05a8463
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-03 14:36:50 -0700

    css before and after content

--- src/slots/Testominal.vue
+++ src/slots/Testominal.vue
@@ -1,16 +1,28 @@
 <template lang="">
     <figure class='mt-20 w-[90%] md:w-[50%] 
-                   mx-auto flex flex-col gap-2'>
+                   mx-auto flex flex-col gap-2
+                   relative -z-50'>
         <blockquote class="border-2 border-green-600 bg-green-500
-                text-slate rounded-3xl pl-10 pr-8 py-12
+                text-slate rounded-3xl pl-10 pr-8 
                 text-2xl
-                before:content-['“'] 
-                after:content-['”']">
+                before:content-['\201C'] 
+                before:font-bold
+                before:text-9xl
+                after:content-['\201D']
+                after:font-bold
+                after:text-9xl
+                after:flex
+                after:justify-end
+              ">
+                
         <slot></slot>
         </blockquote>
         <footer class='text-4xl flex justify-end
-                    text-gray-600 font-semibold'>
-            --<slot name='author'></slot>
+                    text-black font-semibold
+                    
+                   
+                    '>
+            --<slot name='author'>Unknown</slot>
         </footer>    
     </figure>
 </template>
