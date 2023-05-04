428b2b7ab880e7b164f21303f631f236bbaf5e0a
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-03 19:39:23 -0700

    fixed vaildation error and css txt area

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,21 +1,27 @@
 <template lang="" >
     <TitleSction title="Contact"/> 
-    <form class="mx-4 my-20                
+    <form  action="" class="mx-4 my-20                
                     flex flex-col gap-5">
         <label for="subject" class="text-2xl">Subject:</label>
         <input type="text" name="subject" id="subject"
-         placeholder="Your Subject ...✉️" required minlength="3" maxlength="60"
-        class="h-12  border-solid border-2 border-slate-300  rounded-md p-2 text-xl ">
+        placeholder="Your Subject ...✉️" 
+         minlength="3" maxlength="60" required
+        class="h-12  border-solid border-2
+         border-slate-300 rounded-md p-2
+          text-xl" />
         <label for="message" class="text-2xl">Message:</label>
         <textarea name="message" id="message"  cols="30" rows="10"
         placeholder="Message ...🚀" required
-        class="h-12 border border-slate-300 rounded-md p-2 text-xl"></textarea>    
+        class="h-40 border border-slate-300
+             rounded-md p-2 text-xl"></textarea>    
        
-        <input type="button" value="Submit"
-                class="h-10 w-2/6 text-xl
+        <button
+                class="w-2/6 md:w-48 text-xl  font-semibold p-5
                 text-slate-300 mt-5
                 bg-teal-700 hover:bg-teal-600 active:bg-teal-500
-                rounded-md"> 
+                rounded-xl border border-slate-900 border-solid"> 
+                Submit
+        </button>
     </form> 
 </template>
 <script>
