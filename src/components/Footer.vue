8e1b91815bb9c734148ad06bb67600c6dd86046b
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-03 20:28:47 -0700

    better formatting

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,26 @@
 <template lang="">
     <footer id="#footer"
      class="p-5 bg-teal-700 text-white text-base md:text-xl">
-      <section class="grid grid-cols-3">
-        <section id="info">
+      <section class="md:grid md:grid-cols-3">
+        <address>
             Acme Rocket-Powered Proudcts,Inc
             555 Astro Way
-            La Ca 90035
-            Email: inquires@acmerockets.com
-            Phone: (555)555-5555
-        </section>
-        <section class="flex flex-col 
-                        justify-top items-center gap-2">
+            La Ca 90035 <br>
+            Email: 
+            <a href="mailto:inquires@acmerockets.com">inquires@acmerockets.com</a>
+            <br>
+            Phone:
+            <a href="tel:+15555555555">(555)555-5555</a> 
+        </address>
+        <hr class="md:hidden">
+        <section class="flex
+                        md:flex md:flex-col 
+                        md:justify-top md:items-center gap-2">
             <a href="#rockets">Our Rockets</a>
             <a href="#testmonials">Testimonails</a>
             <a href="#contact-us">Contact Us</a>
         </section>
+        <hr class="md:hidden">
         <section class="flex flex-col">
             <span>Copyright &copy; {{currentYear}}</span>
             <span>All Rights Reserved</span>
