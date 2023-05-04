ed01bcd8b97816f9f0972b802783d682cc8d4992
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-03 21:10:58 -0700

    setup media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'widescreen': {'raw' : '(min-aspect-ratio: 3/2' },
+        'tallscreen': {'raw' : '(min-aspect-ratio: 1/2' },
+
+      }
+    },
   },
   plugins: [ ],
 }
