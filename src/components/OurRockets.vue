3018f976804bc1edf137f8e7b1de6e83bc69d6de
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-15 14:18:27 -0700

    h screen class added

--- src/components/OurRockets.vue
+++ src/components/OurRockets.vue
@@ -1,20 +1,23 @@
 <template lang="">
- <TitleSction title="Our Rockets"/> 
- <section class='flex flex-col items-center
-                 sm:flex-row  sm:justify-evenly my-20'>
-    <div v-for='{img,name,price} in rocketsInfo' :key='price'
-    class=" border-2 border-gray-500 rounded-3xl
-          shadow-xl
-          flex flex-col items-center 
-           mx-5 my-5 w-2/3 p-2">   
-              
-         <img :src='img' :alt='name' class="w-1/3">
-         <h5>{{name}}</h5> 
-         <p  class='md:hidden'>{{price.amount}}</p>    
-         <p  class='hidden md:block'>{{price.explanation}}</p>          
-    </div>
-   
- </section>
+     <div class="h-screen">
+
+          <TitleSction title="Our Rockets"/> 
+          <section class='flex flex-col items-center
+                          sm:flex-row  sm:justify-evenly my-20'>
+             <div v-for='{img,name,price} in rocketsInfo' :key='price'
+             class=" border-2 border-gray-500 rounded-3xl
+                   shadow-xl
+                   flex flex-col items-center 
+                    mx-5 my-5 w-2/3 p-2">   
+                       
+                  <img :src='img' :alt='name' class="w-1/3">
+                  <h5>{{name}}</h5> 
+                  <p  class='md:hidden'>{{price.amount}}</p>    
+                  <p  class='hidden md:block'>{{price.explanation}}</p>          
+             </div>
+            
+          </section>
+     </div>
 </template>
 <script>
 export default {
