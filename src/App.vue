7aee889be32e182955e483a573f7453b5f24fda7
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-18 14:08:38 -0700

    impl out of div clicked yet issue on hover cards

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="dark">
+  <div class="dark" @click="IsOutOfMenuClicked">
     <div class="sticky top-0">
-      <Menu>
+      <Menu :IsOutOfMenu="WhetherOutOfMenu">
       </Menu>
     </div>
     <main>
       <Hero id="hero" />
-      <hr  id="rockets"
-      class=" dark:bg-gray-300 
+      <hr id="rockets" class=" dark:bg-gray-300 
                 w-1/2 h-1 rounded mx-auto">
 
       <OurRockets />
@@ -15,12 +14,11 @@
                                    w-1/2 h-1 rounded mx-auto
                                   ">
 
-      <Testmonials/>
+      <Testmonials />
       <hr id="contact-us" class=" dark:bg-gray-300 
                                    w-1/2 h-1 rounded mx-auto
                                   ">
-                                  
-     <Contact/>
+      <Contact />
     </main>
     <Footer></Footer>
   </div>
@@ -43,6 +41,24 @@ export default {
     Testmonials,
     Contact,
     Footer,
+  },
+  data() {
+    return {
+      WhetherOutOfMenu: false,
+    }
+  },
+  methods: {
+    IsOutOfMenuClicked(e) {
+      //  debugger;
+      console.log(e.target.getAttribute('aria-label'))
+      console.log(e.target.parentNode.getAttribute('aria-label'))
+      const locMenu = e.target.getAttribute('aria-label')
+      const parentLocMenu = e.target.parentNode.getAttribute('aria-label')
+      if (locMenu === 'mobile' || parentLocMenu === 'mobile')
+       this.WhetherOutOfMenu = false
+      else
+        this.WhetherOutOfMenu = true
+    }
   }
 }
 
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,75 +1,84 @@
 <template>
-    <div class="flex justify-between bg-green-800
-                   text-gray-300">
-        <div class="flex items-center py-4">
-            <img class="w-8 ml-3" src="/favicon.ico" alt="aceme rocket logo">
-            <h2 class="text-2xl md:text-3xl font-bold
-                                ml-3">
-                <a href="#hero">Acme Rockets</a>
-            </h2>
+    <div>
+        <div class="flex justify-between bg-green-800
+               text-gray-300">
+              
+            <div class="flex items-center py-4">
+                <img class="w-8 ml-3" src="/favicon.ico" alt="aceme rocket logo">
+                <h2 class="text-2xl md:text-3xl font-bold
+                            ml-3">
+                    <a href="#hero">Acme Rockets</a>
+                </h2>
+            </div>
+            <nav class="hidden m-2
+                sm:flex sm:items-center 
+                [&>*]:w-32 [&>*]:text-lg
+                md:[&>*]:text-xl md:font-semibold
+                hover:[&>*]:text-xl hover:[&>*]:border
+                hover:[&>*]:border-sky-500
+                hover:[&>*]:rounded-2xl hover:[&>*]:p-2 hover:[&>*]:mx-2
+                active:[&>*]:bg-blue-700 
+                focus:[&>*]:outline-none focus:[&>*]:ring focus:[&>*]:ring-blue-900
+                focus:[&>*]:rounded-full focus:[&>*]:p-2     
+                [&>*:last-child]:unset-all 
+                [&>*:last-child]:ml-5
+                hover:[&>*:last-child]:text-base hover:[&>*:last-child]:mx-0
+                                     " aria-label="main">
+                <a href="#rockets">Our Rocket</a>
+                <a href="#testmonials">Testmonials</a>
+                <a href="#contact-us">Contact Us</a>
+                <ThemeBtn />
+            </nav>
+            <div :class="{ hidden: !showingMenu }" class=" border-2  sm:hidden
+                m-2 rounded-2xl border-red-900
+                [&>*]:bg-blue-400 [&>*]:w-6 [&>*]:h-1
+                [&>*]:m-2 cursor-pointer
+                hover:[&>*]:w-12 " @click="toggleMenu">
+                <hr />
+                <hr />
+                <hr />
+                <!-- &#9776;  it is unicode for hambergure icon-->
+            </div>
+            <div :class="{ hidden: showingMenu }" class=" border-2 sm:hidden
+                m-2 rounded-2xl border-red-900 [&>*]:bg-blue-400
+                [&>*]:w-10 [&>*]:h-1
+                flex items-center relative
+                 " @click="toggleMenu">
+                <hr class="rotate-45 absolute">
+                <hr class="-rotate-45">
+            </div>
         </div>
-        <nav class="hidden m-2
-                    sm:flex sm:items-center 
-                    [&>*]:w-32 [&>*]:text-lg
-                    md:[&>*]:text-xl md:font-semibold
-                    hover:[&>*]:text-xl hover:[&>*]:border
-                    hover:[&>*]:border-sky-500
-                    hover:[&>*]:rounded-2xl hover:[&>*]:p-2 hover:[&>*]:mx-2
-                    active:[&>*]:bg-blue-700 
-                    focus:[&>*]:outline-none focus:[&>*]:ring focus:[&>*]:ring-blue-900
-                    focus:[&>*]:rounded-full focus:[&>*]:p-2     
-                    [&>*:last-child]:unset-all 
-                    [&>*:last-child]:ml-5
-                    hover:[&>*:last-child]:text-base hover:[&>*:last-child]:mx-0
-                                         " aria-label="main">
+        <nav :class="{ hidden: showingMenu }" class="absolute right-0 bg-green-800 [&>*]:m-3      
+            flex flex-col
+            md:font-semibold sm:hidden sm:[&>*]:hidden
+            [&>*]:text-lg text-gray-300
+            hover:[&>*]:text-xl hover:[&>*]:border hover:[&>*]:border-sky-500
+            hover:[&>*]:rounded-2xl hover:[&>*]:p-2 hover:[&>*]:mx-2
+            active:[&>*]:bg-blue-700 
+            focus:[&>*]:outline-none focus:[&>*]:ring focus:[&>*]:ring-blue-900
+            focus:[&>*]:rounded-full focus:[&>*]:p-2 
+            h-screen
+            " aria-label="mobile">
+            
             <a href="#rockets">Our Rocket</a>
             <a href="#testmonials">Testmonials</a>
             <a href="#contact-us">Contact Us</a>
             <ThemeBtn />
         </nav>
-        <div :class="{ hidden: !showingMenu }" class=" border-2  sm:hidden
-                    m-2 rounded-2xl border-red-900
-                    [&>*]:bg-blue-400 [&>*]:w-6 [&>*]:h-1
-                    [&>*]:m-2 cursor-pointer
-                    hover:[&>*]:w-12 " @click="toggleMenu">
-            <hr />
-            <hr />
-            <hr />
-            <!-- &#9776;  it is unicode for hambergure icon-->
-        </div>
-        <div :class="{ hidden: showingMenu }" class=" border-2 sm:hidden
-                    m-2 rounded-2xl border-red-900 [&>*]:bg-blue-400
-                    [&>*]:w-10 [&>*]:h-1
-                    flex items-center relative
-                     " @click="toggleMenu">
-            <hr class="rotate-45 absolute">
-            <hr class="-rotate-45">
-        </div>
     </div>
-    <nav :class="{ hidden: showingMenu }"
-          class="absolute right-0 bg-green-800 [&>*]:m-3      
-                flex flex-col
-                md:font-semibold sm:hidden sm:[&>*]:hidden
-                [&>*]:text-lg text-gray-300
-                hover:[&>*]:text-xl hover:[&>*]:border hover:[&>*]:border-sky-500
-                hover:[&>*]:rounded-2xl hover:[&>*]:p-2 hover:[&>*]:mx-2
-                active:[&>*]:bg-blue-700 
-                focus:[&>*]:outline-none focus:[&>*]:ring focus:[&>*]:ring-blue-900
-                focus:[&>*]:rounded-full focus:[&>*]:p-2 
-                h-screen
-                " aria-label="mobile">
-        <a href="#rockets">Our Rocket</a>
-        <a href="#testmonials">Testmonials</a>
-        <a href="#contact-us">Contact Us</a>
-        <ThemeBtn />
-    </nav>
 </template>
 <script>
 
 export default {
+    props: ['IsOutOfMenu'],
     data() {
         return {
             showingMenu: true,
+        }
+    },
+    watch:{
+        IsOutOfMenu(newValue){
+            this.showingMenu = newValue;
         }
     },
     methods: {
