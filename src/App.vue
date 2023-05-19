57fa0b2a4b1cd60bffb5f14fff31b37a9ae61ce1
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-18 20:55:00 -0700

    cleaning up

--- src/App.vue
+++ src/App.vue
@@ -49,9 +49,6 @@ export default {
   },
   methods: {
     IsOutOfMenuClicked(e) {
-      //  debugger;
-      console.log(e.target.getAttribute('aria-label'))
-      console.log(e.target.parentNode.getAttribute('aria-label'))
       const locMenu = e.target.getAttribute('aria-label')
       const parentLocMenu = e.target.parentNode.getAttribute('aria-label')
       if (locMenu === 'mobile' || parentLocMenu === 'mobile')
