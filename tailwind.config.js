d1842110b5903b9e95480b995a3ddda821bff604
kickbuttowski80 <pazpaz25@gmail.com>
2023-05-23 13:35:44 -0700

    define new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        papayawhip: {
+          light: '#fef4e4',
+          DEFAULT: '#ffefd5',
+          dark: '#fee5bc',
+        }
+      },
       screens: {
         'widescreen': { 'raw': '(min-aspect-ratio: 3/2' },
         'tallscreen': { 'raw': '(min-aspect-ratio: 13/20' },
